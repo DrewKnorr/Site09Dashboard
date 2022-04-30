0ccba1730e2e1faa24861e2f047a9cfd0aa0efba
Aknorr <aknorr222@gmail.com>
2022-04-29 21:04:29 -0600

    push this shit

--- src/components/pages/researchLogs.js
+++ src/components/pages/researchLogs.js
@@ -19,33 +19,41 @@ export default class RecordLogs extends Component {
       let tempDiv = document.createElement("div");
       tempDiv.className = `log-item`
 
-      let tempId = document.createElement("h2");
-      tempId.innerHTML=`ID: ${response.Logs[i].id}`
+      let tempId = document.createElement("p");
+      tempId.innerHTML=`${response.Logs[i].id}`
+      tempId.className='id-wrapper'
 
-      let temprname = document.createElement("h2");
-      temprname.innerHTML=`Research Name: ${response.Logs[i].rname}`
+      let temprname = document.createElement("p");
+      temprname.innerHTML=`${response.Logs[i].rname}`
+      temprname.className=`name-wrapper`
 
-      let temprrank = document.createElement("h2");
-      temprrank.innerHTML=`Research Rank: ${response.Logs[i].rrank}`
+      let temprrank = document.createElement("p");
+      temprrank.innerHTML=`${response.Logs[i].rrank}`
+      temprrank.className=`rank-wrapper`
 
-      let tempSCP = document.createElement("h2");
-      tempSCP.innerHTML=`SCP's Used: ${response.Logs[i].scpused}`
+      let tempSCP = document.createElement("p");
+      tempSCP.innerHTML=`${response.Logs[i].scpused}`
+      tempSCP.className=`scp-wrapper`
 
-      let approvedBy = document.createElement("h2");
-      tempSCP.innerHTML=`Approved By: ${response.Logs[i].approvedby}`
+      let approvedBy = document.createElement("p");
+      approvedBy.innerHTML=`${response.Logs[i].approvedby}`
+      approvedBy.className=`approved-wrapper`
 
-      let log = document.createElement("h2");
-      log.innerHTML=`Log: ${response.Logs[i].log}`
+      let log = document.createElement("p");
+      log.innerHTML=`${response.Logs[i].log}`
+      log.className=`log-wrapper`
 
-      let timeStamp = document.createElement("h2");
-      timeStamp.innerHTML=`Time: ${response.Logs[i].timeStamp}`
+      let timeStamp = document.createElement("p");
+      timeStamp.innerHTML=`${response.Logs[i].timeStamp}`
+      timeStamp.className=`time-wrapper`
 
-      tempDiv.appendChild(tempId)
-      tempDiv.appendChild(temprname)
-      tempDiv.appendChild(temprrank)
-      tempDiv.appendChild(approvedBy)
-      tempDiv.appendChild(log)
-      tempDiv.appendChild(timeStamp)
+      tempDiv.appendChild(tempId);
+      tempDiv.appendChild(temprname);
+      tempDiv.appendChild(temprrank);
+      tempDiv.appendChild(approvedBy);
+      tempDiv.appendChild(tempSCP);
+      tempDiv.appendChild(log);
+      tempDiv.appendChild(timeStamp);
 
       wrapper.appendChild(tempDiv)
     }
@@ -77,8 +85,8 @@ export default class RecordLogs extends Component {
                   handleSuccessfulLogout={this.props.handleSuccessfulLogout}
               />
           </div>
-          <div id='log-content-wrapper' style={{width:"90vw", height:"90vh"}}>
-            <div id='log-wrapper' style={{width:"90vw", height:"90vh"}}>
+          <div id='log-content-wrapper' >
+            <div id='log-review-wrapper' >
                 <div id='log-contol-wrapper'>
                   <div>
                     <label>Researcher:</label>
@@ -100,9 +108,18 @@ export default class RecordLogs extends Component {
                     <button>TEST</button>
                   </div>
                 </div>
-                <div id='table-wrapper'>
+            </div>
+            <div id='table-wrapper'>
+              <div className='log-item'>
+                <p className='id-wrapper'>ID:</p>
+                <p className='name-wrapper'>Researcher Name:</p>
+                <p className='rank-wrapper'>Researcher Rank:</p>
+                <p className='approved-wrapper'>Approved By:</p>
+                <p className='scp-wrapper'>SCP's Used:</p>
+                <p className='log-wrapper'>Log:</p>
+                <p className='time-wrapper'>Time:</p>
+              </div>
 
-                </div>
             </div>
           </div>
       </div>
