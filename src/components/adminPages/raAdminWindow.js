aa6379a59d97b3f0d2722bd31a27dc87f90c6bdc
Aknorr <aknorr222@gmail.com>
2022-05-12 19:30:23 -0600

    FUCK THIS SHIT

--- src/components/adminPages/raAdminWindow.js
+++ src/components/adminPages/raAdminWindow.js
@@ -7,9 +7,14 @@ import UserControl from '../adminParts/raUserControl.js';
 export default class RaAdminWindows extends Component {
     constructor(props) {
         super(props);
+        this.state={
+            usercontrol:'hidden',
+            logstats:'hidden'
+        }
+        this.handleHide = this.handleHide.bind(this);
       }
 
-    componentDidMount(){
+componentDidMount(){
     fetch("https://site-09-api.herokuapp.com/get/all/users", {
         method: "POST",
         headers: { 'Content-Type': 'application/json'},
@@ -20,22 +25,45 @@ export default class RaAdminWindows extends Component {
     }) 
 }
 
+handleHide(event){
+    console.log("TEST")
+    if(event.target.value=='user'){
+        if(this.state.usercontrol=='hidden'){
+            let userControl = document.createElement(UserControl)
+            this.setState({
+                usercontrol:'visible',
+                logstats:'hidden'
+            })
+        }
+    }
+    else if(event.target.value=='logstats'){
+        if(this.state.logstats=='hidden'){
+            this.setState({
+                usercontrol:'hidden',
+                logstats:'visible'
+            })
+        }
+    }
+
+}
+
+
     
   render() {
     return (
-        <div className='home-wrapper'>
-            <div id='ra-adim-wrapper'>
+        <div className='ra-wrapper'>
+            <div id='ra-admin-wrapper'>
                 <div id='ra-admin-container'>
-                    <div className='admin-item'>
+                    <div className='admin-item' id='user' style={{visibility:this.state.usercontrol}}>
                         <UserControl/>
                     </div>
-                    <div className='admin-item'>
+                    <div className='admin-item' style={{visibility:this.state.logstats}}>
                         <p>Research Log Stats</p>
                     </div>
                 </div>
                 <div id='ra-admin-menu'>
-                    <button>User Control</button>
-                    <button>Research Log Stats</button>
+                    <button value='user' onClick={this.handleHide}>User Control</button>
+                    <button value='logstats' onClick={this.handleHide}>Research Log Stats</button>
                 </div>
             </div>
             <div className='nav-wrapper'style={{zIndex:1,right:0,position:'absolute'}}>
--- src/components/adminParts/raUserControl.js
+++ src/components/adminParts/raUserControl.js
@@ -6,7 +6,75 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 class UserControl extends Component {
     constructor(props) {
         super(props);
+        this.state={
+            errorText:'',
+            newUser:'hidden'
+        }
+
+        this.buildTable = this.buildTable.bind(this);
+        this.newUser = this.newUser.bind(this);
+        this.closeNewUser = this.closeNewUser.bind(this);
       }
+
+    buildTable(users){
+        let table = document.getElementById('users-table');
+        table.innerHTML='';
+
+        let firstTempDiv = document.createElement("div");
+        firstTempDiv.className = `user-item`;
+
+        let firstUserId= document.createElement("p");
+        firstUserId.innerHTML='ID:';
+        firstUserId.className='id-wrapper';
+
+        let firstUsername=document.createElement("p");
+        firstUsername.innerHTML='Username:';
+        firstUsername.className='username-wrapper';
+
+        let firstEmailAuthed=document.createElement("p");
+        firstEmailAuthed.innerHTML='Is Email Authenicated:';
+        firstEmailAuthed.className='email-authed-wrapper';
+
+        let firstEdit=document.createElement("p");
+        firstEdit.innerHTML='';
+        firstEdit.className='edit-user-wrapper';
+
+        firstTempDiv.appendChild(firstUserId);
+        firstTempDiv.appendChild(firstUsername);
+        firstTempDiv.appendChild(firstEmailAuthed);
+        firstTempDiv.appendChild(firstEdit);
+        table.appendChild(firstTempDiv);
+
+        for(let i=0; i<users.Users.length; i++){
+            let tempDiv = document.createElement("div");
+            tempDiv.className = `user-item`;
+    
+            let userId= document.createElement("p");
+            userId.innerHTML=users.Users[i].id;
+            userId.className='id-wrapper';
+    
+            let username=document.createElement("p");
+            username.innerHTML=users.Users[i].username;
+            username.className='username-wrapper';
+    
+            let emailAuthed=document.createElement("p");
+            emailAuthed.innerHTML=users.Users[i].email_authed;
+            emailAuthed.className='email-authed-wrapper';
+
+            let editUser=document.createElement("button");
+            editUser.innerHTML="Edit"
+            editUser.className='edit-user-wrapper';
+    
+            tempDiv.appendChild(userId);
+            tempDiv.appendChild(username);
+            tempDiv.appendChild(emailAuthed);
+            tempDiv.appendChild(editUser);
+            table.appendChild(tempDiv);
+        }
+
+        
+        console.log(users)
+    }
 
     componentDidMount(){
         console.log("test")
@@ -15,16 +83,46 @@ class UserControl extends Component {
         headers: { 'Content-Type': 'application/json'},
     }).then((response) => {
         response.json().then((body) => {
-            console.log(body)
+            if(Object.keys(body)[0]=="errText"){
+                this.setState({
+                  errorText:Object.values(body)
+                })
+              }
+              else{
+                this.buildTable(body);
+                
+              }
         });
     }) 
-}
+    }
+
+    newUser(event){
+      this.setState({
+        newUser:'visible'
+      })
+    }
+    closeNewUser(event){
+      this.setState({
+        newUser:'hidden'
+      })
+    }
+
 
     
   render() {
     return (
-        <div className='home-wrapper'>
-            <div>TEST</div>
+      <div id='user-wrapper'>
+          <div>Admin Users</div>
+          <div id='users-table'>
+
+          </div>
+          <div>
+            <button onClick={this.newUser}>New User</button>
+          </div>
+          <div id='new-user-wrapper' style={{visibility:this.state.newUser}}>
+            <p>NEW USER</p>
+            <button onClick={this.closeNewUser}>X</button>
+          </div>
       </div>
     );
   }
